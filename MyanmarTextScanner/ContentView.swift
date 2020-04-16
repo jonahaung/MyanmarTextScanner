8f4c50f7aa001f92ba813d1010ad156ce2ca9be2
Aung Ko Min <jonahaung@gmail.com>
2020-04-17 06:31:54 +0800

    Initial Commit

---
+++ MyanmarTextScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyanmarTextScanner
+//
+//  Created by Aung Ko Min on 17/4/20.
+//  Copyright © 2020 Aung Ko Min. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
